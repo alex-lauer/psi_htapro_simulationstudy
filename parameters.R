41b3588c04eb6199706fbd3ada4fbe775d0826c8
Evgeniia <evgeniia.artamonova@boehringer-ingelheim.com>
2026-02-16 14:08:24 +0000

    Initial commit, definition of parameters for scenarios

---
+++ parameters.R
@@ -0,0 +1,10 @@
+#### Parameters for PRO model for simulation scenarios
+
+#### Fixed parameters ####
+
+mu_P0   <- 70          # mean baseline PRO
+sd_P0   <- 20          # sd baseline PRO
+beta0   <- 0           # intercept
+beta2   <- 0           # no baseline treatment offset
+beta4   <- 2/70       # baseline-PRO effect on slope
+
